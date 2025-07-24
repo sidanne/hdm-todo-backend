373b243a062f6c96b2ad437dc362c0358d0a1336
sidannen <sidanne@eafc-uccle.be>
2025-07-24 19:11:50 +0200

    Sauvegarde temporaire avant rebase

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // ✅ Autorise le frontend React à accéder à l'API
-  app.enableCors({
-    origin: 'http://localhost:5173', // autorise le frontend
-    credentials: true,
-  });
-
+  const app = await NestFactory.create(AppModule, { cors: true }); // CORS activé
   await app.listen(3000);
 }
 bootstrap();
